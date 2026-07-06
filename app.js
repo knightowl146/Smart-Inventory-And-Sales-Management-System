e145fce61e57dad9c3686ddf8c66173c3667d068
Mridul <mridulvyas08@gmail.com>
2026-07-07 01:45:57 +0530

    initializing server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//middlewares
+app.use(express.urlencoded({extended: true}));
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+
+
+app.get("/", (req,res)=>{
+    res.send("Welcome to Smart Inventory API");
+})
+
+//Export the app instance for use in server.js
+module.exports = app;
