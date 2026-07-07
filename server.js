c6f488b19a148a4f3487bc0598eeda3a40adc79e
Mridul <mridulvyas08@gmail.com>
2026-07-08 01:48:57 +0530

    Initializing server

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const PORT = process.env.PORT;
+
+
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/inventory');
+    console.log("Database connected");
+}
 
 
 app.listen(PORT, () => {
