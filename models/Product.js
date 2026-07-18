07f38dde6bf6df3fb6bff07e4659eb21af85f95d
Mridul <mridulvyas08@gmail.com>
2026-07-18 22:41:20 +0530

    Model Initialization

---
+++ models/Product.js
@@ -0,0 +1,44 @@
+const productSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 100,
+    },
+
+    sku:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    category:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    purchasePrice: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    sellingPrice:{
+        type: Number,
+        required: true,
+        min: 0
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0,
+        default: 0
+    },
+    description:{
+        type:String,
+        required: true
+    }
+},{timestamps: true});
+
+module.exports = mongoose.model('Product', productSchema);
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const app = require("./app");
-const mongoose = require("mongoose");
 require("dotenv").config();
-const PORT = process.env.PORT;
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 3000;
 
 
+async function startServer() {
+    await connectDB();
 
-async function main() {
-    await mongoose.connect('mongodb://localhost:27017/inventory');
-    console.log("Database connected");
+    app.listen(PORT, () => {
+        console.log(`The server is running on port ${PORT}`);
+    });
 }
 
-
-app.listen(PORT, () => {
-    console.log(`The server is running on port ${PORT}`);
+startServer().catch((err) => {
+    console.log(err);
 });
+
