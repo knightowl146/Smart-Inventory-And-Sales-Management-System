90dceecddb12acc067ecb4c959277befa9d89a8e
Mridul <mridulvyas08@gmail.com>
2026-07-19 22:01:24 +0530

    CRUD operations for products

---
+++ controllers/productController.js
@@ -0,0 +1,248 @@
+const Product = require("../models/Product");
+
+const createProduct = async (req,res) =>{
+
+    try{
+        // Destructure req.body
+        const {name, sku, category,purchasePrice,sellingPrice,quantity,description} = req.body;
+        // Validation
+        if(!name || !sku || !category || purchasePrice==null || sellingPrice==null || quantity == null || !description){
+            return res.status(400).json({
+                success: false,
+                message:"All fields are required"
+            });
+        }
+        // MongoDB query to create a new product
+        const product = await Product.create({
+            name,
+            sku,
+            category,
+            purchasePrice,
+            sellingPrice,
+            quantity,
+            description
+        });
+        // Respond with success message
+        res.status(201).json({
+            success: true,
+            message: "Product created successfully",
+            data: product
+        });
+    }catch(error){
+        // Error handling
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server error"
+        });
+    }
+}
+
+const getProducts = async (req,res) =>{
+    try{     
+        const products = await Product.find();
+
+        return res.status(200).json({
+            success: true,
+            message: "Products fetched successfully",
+            data: products
+        })
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }   
+}
+
+const getProductById = async(req,res) =>{
+
+  try{
+
+    const {id} = req.params;
+    //Validating id
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if(!isValid){
+      return res.status(400).json({
+        success: false,
+        message: "Invalid id",
+      });
+    }
+    const product = await Product.findById(id);
+
+    // Product doesn't exist
+    if(!product){
+      return res.status(404).json({
+        success: false,
+        message: "Product doesn't exist"
+      });
+    }
+    //Product found 
+    return res.status(200).json({
+      success: true,
+      message: "Product found!",
+      data: product
+    });
+  }
+  catch(err){
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
+}
+
+const updateProduct = async (req,res) =>{
+
+  try{
+    const isValid = mongoose.Types.ObjectId.isValid(req.params.id);
+    if(!isValid){
+      return res.status(404).json({
+        success: false,
+        message: "Invalid id"
+      })
+    };
+    const allowedUpdates = new Set([
+      "name",
+      "price",
+      "quantity",
+      "category",
+      "description"
+    ]);
+    const updates = Object.keys(req.body);
+    const isValidUpdate = updates.every( (field)=> allowedUpdates.has(field));
+    if(!isValidUpdate){
+      return res.status(400).json({
+        success: false,
+        message: "Invalid updates"
+      })
+    };
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true
+      }
+    );
+  
+    if(!product){
+      return res.status(400).json({
+      success: false,
+      message: "Product not found"
+    })
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Product updated",
+      data: product
+    });
+  }
+catch(err){
+  console.error(err);
+  return res.status(500).json({
+    success: false,
+    message: "Internal server error"
+  });
+  }
+}
+const deleteProduct = async (req,res)=>{
+  try{
+    const {id} = req.params;
+    const isValid = mongoose.Types.ObjectId.isValid(id);
+    if(!isValid){
+      return res.status(400).json({
+        success: false,
+        message: "Invalid id"
+      });
+    }
+    const product = await Product.findByIdAndDelete(id);
+    if(!product){
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted",
+      data: product
+    });
+  }
+  catch(err){
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  };
+};
+
+const purchaseStock = async (req,res)=>{
+
+
+  try{
+    const {quantity} = req.body;
+    const {id} = req.params;
+    const isValidId = mongoose.Types.ObjectId.isValid(id);
+    if(!isValidId){
+      return res.status(400).json({
+        success: false,
+        message: "Invalid id"
+      });
+    }
+    if(typeof quantity !== 'number' || isNaN(quantity)){
+      return res.status(400).json({
+        success: false,
+        message: "Quantity is required and must be a number"
+      });
+
+    }
+    if(quantity <= 0){
+      return res.status(400).json({
+        success: false,
+        message: "Quantity must be greater than 0"
+      });
+    }
+    if(!Number.isInteger(quantity)){
+      return res.status(400).json({
+        success: false,
+        message: "Quantity must be an integer"
+      });
+    }
+    
+    const product = await Product.findByIdAndUpdate(
+        id,
+        {
+            $inc: {
+                quantity: quantity
+            }
+        },
+        {
+          new: true,
+          runValidators: true
+        }
+    )
+    if(!product){
+        return res.status(404).json({
+            success:false,
+            message:"Product not found"
+        });
+    }
+    return res.status(200).json({
+        success:true,
+        message: "Stock added successfully",
+        data: product
+    });
+    
+  }
+  catch(err){
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  };
+};
